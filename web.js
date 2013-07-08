bfed4b5300f38aee3b7532a2dd534234c0966153
Farid Hasanov <hasanov.farid88@gmail.com>
2013-07-08 22:03:39 +0000

    Index

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.get('/', function(request, response) {
   var buf = new Buffer (256); 
 
 var fs = require('fs');
-   response.send(buf.toString());
+   response.send(fs.readFileSync('index.html').toString());
 //  response.send('Hello World2!');
  });
 var port = process.env.PORT || 5000;app.listen(port, function() {
