36abfe04c2393acc71b3405247f9c53370711f9f
Farid Hasanov <hasanov.farid88@gmail.com>
2013-07-08 21:18:53 +0000

    Index

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
 
-wr = fs.readFileSync('index.html');
-buf = new Buffer(256);
-
 app.get('/', function(request, response) {
-  response.send(buf.toString('utc8', fs.readFileSync('index.html')));
-});
+var fs = require('fs');
+var buff = new buffer ();
+  response.send(buff.toString('utc 8', fs.readFileSync("index.html")))});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
-
