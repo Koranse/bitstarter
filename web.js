5025493ecc3a16e2f3c1e5ff3715924e28b1bab1
Farid Hasanov <hasanov.farid88@gmail.com>
2013-07-08 21:22:18 +0000

    Index

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buff = newBuffer ();
+var buff = new Buffer ();
   response.send(buff.toString('utc 8', fs.readFileSync("index.html")))});
 
 
